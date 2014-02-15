6cb707790c9bb945c86a0e88b616f1531ce905fa
Toby Hinloopen <toby@kutcomputers.nl>
2014-02-15 11:45:30 +0100

    Een geel lijntje.

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-function main() {
-  var canvas = document.getElementById("canvas");
-  var c2d = canvas.getContext("2d");
+var canvas;
+var c2d;
 
-  c2d.fillStyle = "#222";
-  c2d.fillRect(0, 0, 640, 480);
+function render() {
+  c2d.clearRect(0, 0, 640, 480);
+
+  c2d.strokeStyle = "yellow";
+
+  c2d.beginPath();
+  c2d.moveTo(320.5, 480.5);
+  c2d.lineTo(320.5, 240.5);
+  c2d.lineTo(360.5, 240.5);
+  c2d.lineTo(360.5, 320.5);
+  c2d.lineTo(330.5, 320.5);
+  c2d.stroke();
+
+  c2d.fillStyle = "yellow";
+  c2d.fillRect(330, 319, 7, 3);
+}
+
+function main() {
+  canvas = document.getElementById("canvas");
+  c2d = canvas.getContext("2d");
+  render();
 }
 
 document.addEventListener("DOMContentLoaded", main);
