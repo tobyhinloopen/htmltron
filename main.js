24dba9b6524046b7f0c48bc2a1ca372de3089593
Toby Hinloopen <toby@kutcomputers.nl>
2014-02-16 16:14:40 +0100

    De eerste bestuurbare speler (1/2)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var canvas;
 var c2d;
 var current_time;
+var player;
 
 function render() {
   var x = (current_time % 500)/500*30;
@@ -10,11 +11,10 @@ function render() {
   c2d.strokeStyle = "yellow";
 
   c2d.beginPath();
-  c2d.moveTo(320.5, 480.5);
-  c2d.lineTo(320.5, 240.5);
-  c2d.lineTo(360.5, 240.5);
-  c2d.lineTo(360.5, 320.5);
-  c2d.lineTo(350.5 - x, 320.5);
+  c2d.moveTo(player.path[0].x + 0.5, player.path[0].y + 0.5);
+  for(var i=1; i<player.path.length; i++) {
+    c2d.lineTo(player.path[i].x + 0.5, player.path[i].y + 0.5);
+  }
   c2d.stroke();
 
   c2d.fillStyle = "yellow";
@@ -24,13 +24,18 @@ function render() {
 function main() {
   canvas = document.getElementById("canvas");
   c2d = canvas.getContext("2d");
+  player = new Player();
+  player.spawn({ x: 320, y: 440 });
+  Mousetrap.bind("z", function() { player.left();  });
+  Mousetrap.bind("x", function() { player.right(); });
   current_time = null;
   requestAnimationFrame(next);
 }
 
 function next(t) {
-  var delta = current_time == null ? 0 : (t - current_time);
+  var delta = current_time == null ? 0 : (t - current_time) / 1000;
   requestAnimationFrame(next);
+  player.update(delta);
   render();
   current_time = t;
 }
