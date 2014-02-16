7f5b671a87f62eec15c3aa3503b30e04cca830e6
Toby Hinloopen <toby@kutcomputers.nl>
2014-02-16 16:24:23 +0100

    Bughunting

--- player.js
+++ player.js
@@ -13,10 +13,12 @@ Player.MOVEMENTS = [
 
 Player.prototype.right = function() {
   this.direction++;
+  this.add_point();
 };
 
 Player.prototype.left = function() {
-  this.direction--;
+  this.direction+=3;
+  this.add_point();
 };
 
 Player.prototype.get_movement = function() {
@@ -27,14 +29,19 @@ Player.prototype.get_current_position = function() {
   return this.path[this.path.length-1];
 };
 
+Player.prototype.add_point = function() {
+  var p = this.get_current_position();
+  this.path.push({ x: p.x, y: p.y });
+}
+
 Player.prototype.spawn = function(p) {
   this.path.push({ x: p.x, y: p.y }); // initial point
-  this.path.push({ x: p.x, y: p.y }); // 2nd & current point, to make a line
+  this.add_point(); // 2nd & current point, to make a line
 };
 
 Player.prototype.update = function(delta) {
   var pos = this.get_current_position();
   var mov = this.get_movement();
-  pos.x += mov.x * this.max_speed;
-  pos.y += mov.y * this.max_speed;
+  pos.x += mov.x * this.max_speed * delta;
+  pos.y += mov.y * this.max_speed * delta;
 };
