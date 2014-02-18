98900e683c768704ca26dba3dc155c973e847e91
Toby Hinloopen <toby@kutcomputers.nl>
2014-02-18 09:14:28 +0100

    multiplayer starting

--- main.js
+++ main.js
@@ -1,46 +1,11 @@
-var canvas;
-var c2d;
-var current_time;
-var player;
+var connect = require('connect');
+var http = require('http');
+var io = require('socket.io');
+var app = connect().use(connect.static("./client"));
+var server = http.createServer(app);
+var io = io.listen(server);
+server.listen(3000);
 
-function render() {
-  c2d.clearRect(0, 0, 640, 480);
-
-  c2d.strokeStyle = "yellow";
-
-  c2d.beginPath();
-  c2d.moveTo(player.path[0].x + 0.5, player.path[0].y + 0.5);
-  for(var i=1; i<player.path.length; i++) {
-    c2d.lineTo(player.path[i].x + 0.5, player.path[i].y + 0.5);
-  }
-  c2d.stroke();
-
-  c2d.fillStyle = "yellow";
-  var p = player.get_current_position();
-  if(player.direction % 2 == 1) {
-    c2d.fillRect(p.x - 3, p.y - 1, 7, 3);
-  } else {
-    c2d.fillRect(p.x - 1, p.y - 3, 3, 7);
-  }
-}
-
-function main() {
-  canvas = document.getElementById("canvas");
-  c2d = canvas.getContext("2d");
-  player = new Player();
-  player.spawn({ x: 320, y: 440 });
-  Mousetrap.bind("z", function() { player.left();  });
-  Mousetrap.bind("x", function() { player.right(); });
-  current_time = null;
-  requestAnimationFrame(next);
-}
-
-function next(t) {
-  t = t/1000; // convert to seconds
-  requestAnimationFrame(next);
-  player.update(current_time == null ? t : current_time, t);
-  render();
-  current_time = t;
-}
-
-document.addEventListener("DOMContentLoaded", main);
+io.sockets.on("connection", function(socket) {
+  console.log("connection!", socket);
+});
